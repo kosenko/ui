d61a324d5f7ca9053e4d42d32b34bb12de423b80
Kolya Kosenko <kolya.kosenko@gmail.com>
2017-10-04 09:47:08 +0300

    Fix Boost.ASIO chat client example description

--- example/asio_chat_client.cpp
+++ example/asio_chat_client.cpp
@@ -4,8 +4,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See http://www.boost.org/LICENSE_1_0.txt
 
-// GUI implementation of Boost.ASIO chat example.
-// Start server from Boost.ASIO examples.
+// GUI implementation of Boost.ASIO chat client example.
+// Start chat server from Boost.ASIO examples.
 
 #include <cstdlib>
 #include <deque>
