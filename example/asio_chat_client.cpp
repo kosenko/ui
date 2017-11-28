246c434bef3c740b1af52a42d6e4d56f87da8983
Kolya Kosenko <kolya.kosenko@gmail.com>
2017-11-29 00:40:04 +0200

    Add error handling to Boost.ASIO chat example

--- example/asio_chat_client.cpp
+++ example/asio_chat_client.cpp
@@ -20,6 +20,8 @@ namespace ui = boost::ui;
 
 class chat_client
 {
+    typedef chat_client this_type;
+
 public:
     chat_client(boost::function<void(const std::string&)> output_fn)
         : m_socket(m_io_service), m_output_fn(output_fn)
@@ -38,10 +40,10 @@ public:
         boost::asio::ip::tcp::resolver::iterator iterator = resolver.resolve(query);
 
         boost::asio::async_connect(m_socket, iterator,
-            boost::bind(&chat_client::handle_connect, this,
+            boost::bind(&this_type::handle_connect, this,
                 boost::asio::placeholders::error));
 
-        m_thread = boost::thread(boost::bind(&boost::asio::io_service::run, &m_io_service));
+        m_thread = boost::thread(boost::bind(&this_type::run, this));
     }
 
     void write(const std::string& str)
@@ -51,13 +53,25 @@ public:
         msg.body_length(actual_str.size());
         std::memcpy(msg.body(), actual_str.c_str(), msg.body_length());
         msg.encode_header();
-        m_io_service.post(boost::bind(&chat_client::do_write, this, msg));
+        m_io_service.post(boost::bind(&this_type::do_write, this, msg));
     }
 
 private:
+    void run()
+    {
+        try
+        {
+            m_io_service.run();
+        }
+        catch ( std::exception& e )
+        {
+            ui::log::error() << e.what();
+        }
+    }
+
     void close()
     {
-        m_io_service.post(boost::bind(&chat_client::do_close, this));
+        m_io_service.post(boost::bind(&this_type::do_close, this));
         m_thread.join();
     }
 
@@ -67,9 +81,10 @@ private:
         {
             boost::asio::async_read(m_socket,
                 boost::asio::buffer(m_read_msg.data(), chat_message::header_length),
-                boost::bind(&chat_client::handle_read_header, this,
+                boost::bind(&this_type::handle_read_header, this,
                     boost::asio::placeholders::error));
         }
+        else on_error(error);
     }
 
     void handle_read_header(const boost::system::error_code& error)
@@ -78,11 +93,10 @@ private:
         {
             boost::asio::async_read(m_socket,
                 boost::asio::buffer(m_read_msg.body(), m_read_msg.body_length()),
-                boost::bind(&chat_client::handle_read_body, this,
+                boost::bind(&this_type::handle_read_body, this,
                     boost::asio::placeholders::error));
         }
-        else
-            do_close();
+        else do_close(error);
     }
 
     void handle_read_body(const boost::system::error_code& error)
@@ -95,11 +109,10 @@ private:
 
             boost::asio::async_read(m_socket,
                 boost::asio::buffer(m_read_msg.data(), chat_message::header_length),
-                boost::bind(&chat_client::handle_read_header, this,
+                boost::bind(&this_type::handle_read_header, this,
                     boost::asio::placeholders::error));
         }
-        else
-            do_close();
+        else do_close(error);
     }
 
     void do_write(chat_message msg)
@@ -112,7 +125,7 @@ private:
             boost::asio::async_write(m_socket,
                 boost::asio::buffer(m_write_msgs.front().data(),
                     m_write_msgs.front().length()),
-                boost::bind(&chat_client::handle_write, this,
+                boost::bind(&this_type::handle_write, this,
                     boost::asio::placeholders::error));
         }
     }
@@ -128,17 +141,31 @@ private:
                 boost::asio::async_write(m_socket,
                     boost::asio::buffer(m_write_msgs.front().data(),
                     m_write_msgs.front().length()),
-                    boost::bind(&chat_client::handle_write, this,
+                    boost::bind(&this_type::handle_write, this,
                         boost::asio::placeholders::error));
             }
         }
-        else
-            do_close();
+        else do_close(error);
     }
 
     void do_close()
     {
         m_socket.close();
+    }
+
+    void do_close(const boost::system::error_code& error)
+    {
+        do_close();
+        on_error(error);
+    }
+
+    void on_error(const boost::system::error_code& error)
+    {
+        std::ostringstream ss;
+        ss << error.message();
+        ss << "\n\nError code: " << error.value();
+        ss << ", category: " << error.category().name();
+        ui::log::error() << ss.str();
     }
 
 private:
@@ -208,6 +235,8 @@ asio_dialog::asio_dialog() : ui::dialog(
             .on_press(&this_type::on_clear_output, this)
             .tooltip("Clears output text")
         ;
+
+    resize(500, 400);
 }
 
 void asio_dialog::on_connect()
