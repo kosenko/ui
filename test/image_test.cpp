84644b17cad2f318743d68ee2f7dd4d85845b536
Kolya Kosenko <kolya.kosenko@gmail.com>
2017-12-26 07:57:02 +0200

    Test image::native_handle()

--- test/image_test.cpp
+++ test/image_test.cpp
@@ -16,6 +16,7 @@ int ui_main(int argc, char* argv[])
     {
         ui::image img;
         BOOST_TEST(!img.valid());
+        BOOST_TEST(img.native_handle());
         BOOST_TEST_THROWS(img.width(), std::runtime_error);
         BOOST_TEST_THROWS(img.height(), std::runtime_error);
 
@@ -27,23 +28,28 @@ int ui_main(int argc, char* argv[])
 
         img.load(fs);
         BOOST_TEST(img.valid());
+        BOOST_TEST(img.native_handle());
         BOOST_TEST_EQ(img.width(),  16);
         BOOST_TEST_EQ(img.height(), 16);
 
         ui::image img2 = img;
         BOOST_TEST(img2.valid());
+        BOOST_TEST(img2.native_handle());
 
         img2 = ui::image();
         BOOST_TEST(!img2.valid());
+        BOOST_TEST(img2.native_handle());
 
         std::istringstream ss;
         BOOST_TEST_THROWS(img.load(fs), std::runtime_error);
         BOOST_TEST(!img.valid());
+        BOOST_TEST(img.native_handle());
     }
 
     {
         const ui::image img = ui::image::xdg("folder", 32, 32);
         BOOST_TEST(img.valid());
+        BOOST_TEST(img.native_handle());
         BOOST_TEST_EQ(img.width(),  32);
         BOOST_TEST_EQ(img.height(), 32);
     }
